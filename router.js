0c45baa9e82a033a59c1db4ce073ed00338296de
Richard Munroe <rimunroe@gmail.com>
2015-02-18 11:21:24 -0500

    allow instantiation of nested routes

---
+++ router.js
@@ -0,0 +1,50 @@
+function Router(){
+  this.createRoute = function(options /*, children */){
+    var route = {};
+    route.path = (options.path == null) ? '/' : options.path;
+    route.name = options.name;
+    route.handler = options.handler;
+
+    function isParam(segment){
+      return segment[0] === ':';
+    }
+
+    function getParams(path){
+      var segments = path.split('/');
+      // var segments = (splitPath.indexOf('/') > 0) ? splitPath.slice(1, splitPath.length) : splitPath;
+
+      var i;
+      var positions = {};
+      for(i = 0; i < segments.length; i++){
+        var segment = segments[i];
+
+        if (segment[0] === ':') {
+          positions[segment.slice(1, segment.length)] = i;
+        }
+      }
+      return positions;
+    }
+
+    route.params = getParams(route.path);
+
+    var routes = [route];
+    var i;
+
+    for(i = 0; i < arguments.length; i++){
+      var childRoutesBundle = arguments[i];
+
+      var j;
+      for(j = 0; j < childRoutesBundle.length; j++){
+        var childRoute = childRoutesBundle[j];
+        childRoute.path = route.path + ((route.path !== '/') ? '/' : '') + childRoute.path;
+        childRoute.params = getParams(childRoute.path);
+        routes.push(childRoute);
+      }
+    }
+
+    return routes;
+  }
+
+}
+
+module.exports = Router;
