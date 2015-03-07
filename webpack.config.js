6bba4cf1dba7d01b370de941c478e28ea2922300
Richard Munroe <rimunroe@gmail.com>
2015-03-07 17:18:01 -0500

    moved source into src/

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 var path = require('path');
 
 module.exports = {
-  entry: path.join(__dirname, "index.js"),
+  entry: path.join(__dirname, "src", "index.js"),
   output: {
-    filename: 'pipeline-router.js',
+    filename: path.join("build", "pipeline-router.js"),
     libraryTarget: "umd",
     library: "pipeline-router"
   }
