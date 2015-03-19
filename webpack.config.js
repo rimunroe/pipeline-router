f019fc1ea393f49851caffa857c304a74a88317f
Richard Munroe <rimunroe@gmail.com>
2015-03-19 15:15:46 -0400

    added webpack build script and fixed filename

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,9 @@ var path = require('path');
 module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
   output: {
-    filename: path.join("build", "pipeline-router.js"),
+    filename: path.join("build", "pipeline-routing.js"),
     libraryTarget: "umd",
-    library: "pipeline-router"
-  }
+    library: "pipelineRouting"
+  },
+  plugins: []
 };
